c72421f49eab16a4380b12428cfd6fd39a93cc1f
kelvink17 <kelvinkokorie@example.com>
2025-07-23 18:58:53 +0100

    Initial commit

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
+ app.use(cors());
 
 // ✅ Correct CORS config
 app.use(cors({
@@ -46,3 +47,51 @@ app.post('/convert', upload.single('video'), (req, res) => {
 app.listen(5000, () => {
   console.log('✅ Server running on http://localhost:5000');
 });
+
+
+
+ // server/server.js
+// const express = require('express');
+// const multer = require('multer');
+// const ffmpeg = require('fluent-ffmpeg');
+// const fs = require('fs');
+// const cors = require('cors');
+
+// app.use(cors({
+//   origin: 'https://http://mp4-to-mp3-front-end-nu.vercel.app/',
+// }));
+
+// const app = express();
+// app.use(cors());
+
+// const upload = multer({ dest: 'uploads/' });
+// const path = require('path');
+
+// const outputDir = path.join(__dirname, 'converted');
+// if (!fs.existsSync(outputDir)) {
+//   fs.mkdirSync(outputDir);
+// }
+
+
+// app.post('/convert', upload.single('video'), (req, res) => {
+//   const inputPath = req.file.path;
+//   const outputPath = `converted/${req.file.filename}.mp3`;
+
+//   ffmpeg(inputPath)
+//     .toFormat('mp3')
+//     .on('end', () => {
+//       res.download(outputPath, 'converted.mp3', () => {
+//         fs.unlinkSync(inputPath);
+//         fs.unlinkSync(outputPath);
+//       });
+//     })
+//     .on('error', (err) => {
+//       console.error('Conversion error:', err);
+//       res.status(500).send('Conversion failed');
+//     })
+//     .save(outputPath);
+// });
+
+// app.listen(5000, () => {
+//   console.log('✅ Server running on http://localhost:5000');
+// });
